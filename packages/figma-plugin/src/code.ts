ede5f76a2aba3a8fd875179445888470e4b68e82
SoYoung210 <ethdud1@gmail.com>
2022-02-02 20:58:28 +0900

    feat(figma-plugin): plugin poc

--- packages/figma-plugin/src/code.ts
+++ packages/figma-plugin/src/code.ts
@@ -1,36 +1,29 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+import walk from './actions/walk';
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
+
 figma.ui.onmessage = msg => {
+  const numberOfRectangles = 5;
+
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
+  if (msg.type === 'create-skeleton') {
     const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
+    console.log('@@ selection', figma.currentPage.selection);
+    console.log('result: ', walk(figma.currentPage.selection));
+    for (let i = 0; i < numberOfRectangles; i++) {
       const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+      // rect.x = i * 150;
+      // rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+      // figma.currentPage.appendChild(rect);
+      // nodes.push(rect);
     }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
-  console.log(msg.type);
-  if (msg.type === 'selection') {
-    console.log(figma.currentPage.selection);
+
+    // figma.currentPage.selection = nodes;
+    // figma.viewport.scrollAndZoomIntoView(nodes);
   }
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  // figma.closePlugin();
 };
--- packages/figma-plugin/src/index.tsx
+++ packages/figma-plugin/src/index.tsx
@@ -1,9 +1,45 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  HStack,
+  Radio,
+  RadioGroup,
+  VStack,
+  ChakraProvider,
+} from '@chakra-ui/react';
+import React, { useCallback } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return <div>hello</div>;
+  const onCreate = useCallback(() => {
+    parent.postMessage({ pluginMessage: { type: 'create-skeleton' } }, '*');
+  }, []);
+
+  return (
+    <VStack justifyContent="space-between">
+      <FormControl as="fieldset">
+        <FormLabel as="legend">
+          <RadioGroup
+            defaultValue="wave"
+            onChange={value => console.log('value', value)}
+          >
+            <HStack spacing={4}>
+              <Radio value="wave">wave</Radio>
+              <Radio value="pulse">pulse</Radio>
+            </HStack>
+          </RadioGroup>
+        </FormLabel>
+      </FormControl>
+      <Button onClick={onCreate}>Create</Button>
+    </VStack>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById('figma-react-template'));
+ReactDOM.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+  document.getElementById('figma-react-template')
+);
