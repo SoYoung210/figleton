580ffcaf90b1b3ce59a8160d7309a42c4d3d68fa
SoYoung210 <ethdud1@gmail.com>
2022-02-03 01:33:16 +0900

    feat(figma-plugin): to html string

--- packages/figma-plugin/src/actions/string.ts
+++ packages/figma-plugin/src/actions/string.ts
@@ -1,27 +1,25 @@
 import { NodeElement } from '../model';
+import { NodeConstants } from './node';
 
 function toHtmlString(targetNode: NodeElement, rootNode: NodeElement): string {
   const isRootNode = targetNode.id === rootNode.id;
-
-  const isRootFirstChild = rootNode.children?.some(
+  const isFirstChild = rootNode.children?.some(
     rootChild => rootChild.id === targetNode.id
   );
+  const nextValidChildren = targetNode.children?.filter(
+    ({ type }) => !NodeConstants.unsupportedTypes.includes(type)
+  );
+  const hasChildren = nextValidChildren != null;
 
   const position = isRootNode ? 'relative' : 'absolute';
   const { x, y, width, height } = targetNode.renderBounds;
-  const top = isRootNode
-    ? 0
-    : isRootFirstChild
-    ? y - rootNode.renderBounds.y
-    : y;
-  const left = isRootNode
-    ? 0
-    : isRootFirstChild
-    ? x - rootNode.renderBounds.x
-    : x;
+  const top = isRootNode ? 0 : isFirstChild ? y - rootNode.renderBounds.y : y;
+  const left = isRootNode ? 0 : isFirstChild ? x - rootNode.renderBounds.x : x;
 
-  return `<div style={{ position: '${position}', top: ${top}, left: ${left}, width: ${width}, height: ${height}, border: '1px solid red' }}>${(
-    targetNode.children ?? []
+  const positionStyleString = `position: '${position}', top: ${top}, left: ${left}, width: ${width}, height: ${height}`;
+
+  return `<div style={{ ${positionStyleString}, border: '1px solid red' }}>${(
+    nextValidChildren ?? []
   )
     .map(nextNode => toHtmlString(nextNode, rootNode))
     .join('')}</div>`;
