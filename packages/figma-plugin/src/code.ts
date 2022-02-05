c6096be82312051e7785a7d16f6641d3ddb66f50
SoYoung210 <ethdud1@gmail.com>
2022-02-05 22:19:59 +0900

    chore(figma-plugin): code.ts

--- packages/figma-plugin/src/code.ts
+++ packages/figma-plugin/src/code.ts
@@ -1,17 +1,13 @@
 import walk from './actions/walk';
 import { CodedError, PluginMessage } from './model';
 
-figma.showUI(__html__, { height: 250 });
+figma.showUI(__html__, { width: 450, height: 550 });
 
 figma.ui.onmessage = (msg: PluginMessage) => {
-  const numberOfRectangles = 5;
-
   try {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
     if (msg.type === 'create-skeleton') {
-      // TODO: add msg.option
-      const result = walk(figma.currentPage.selection);
+      const result = walk(figma.currentPage.selection, msg.option);
+
       figma.ui.postMessage(
         {
           type: 'preview-code',
@@ -19,16 +15,13 @@ figma.ui.onmessage = (msg: PluginMessage) => {
         },
         { origin: '*' }
       );
-
-      // figma.currentPage.selection = nodes;
-      // figma.viewport.scrollAndZoomIntoView(nodes);
     }
   } catch (e) {
     if (e instanceof CodedError) {
-      figma.closePlugin(e.message);
+      return figma.notify(e.message);
     }
+
+    console.log('e', e);
+    figma.closePlugin('Unexpected Error');
   }
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  // figma.closePlugin();
 };
