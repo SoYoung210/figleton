0ae08fad01919a46fe88d336f6a6ef438e3b04ea
SoYoung210 <ethdud1@gmail.com>
2022-02-05 21:51:15 +0900

    feat(figma-plugin): code preveiw

--- packages/figma-plugin/src/components/CodePreviewSection.tsx
+++ packages/figma-plugin/src/components/CodePreviewSection.tsx
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  useClipboard,
+  Spinner,
+  Stack,
+  Skeleton,
+} from '@chakra-ui/react';
+import React, { useRef, useState } from 'react';
 import { getHighlighter, setCDN } from 'shiki';
+import styled from '@emotion/styled';
 import useAsyncHandler from '../hooks/async';
 import useEventListener from '../hooks/event';
 
 export default function CodePreviewSection() {
   setCDN('https://unpkg.com/shiki/');
 
-  const [codeResult, setCodeResult] = useState('');
+  const [highlightText, setHighlightText] = useState('');
+  const originDataRef = useRef<string>('');
+  const { onCopy, hasCopied } = useClipboard(originDataRef.current);
+
+  const highlighter = getHighlighter({ theme: 'dark-plus' });
 
   const [handleData, { isPending }] = useAsyncHandler(
     async (payload: string) => {
-      console.log('code?', payload);
-      const highlighter = await getHighlighter({ theme: 'dark-plus' });
-      console.log('highlighter', highlighter);
-      setCodeResult(
-        highlighter.codeToHtml(payload, {
+      setHighlightText(
+        (await highlighter).codeToHtml(payload, {
           lang: 'jsx',
         })
       );
@@ -22,13 +32,43 @@ export default function CodePreviewSection() {
   );
 
   useEventListener<string>({ type: 'preview-code' }, e => {
-    console.log('code preveiw', e.data.pluginMessage.payload);
+    originDataRef.current = e.data.pluginMessage.payload;
     handleData(e.data.pluginMessage.payload);
   });
 
   return (
-    <pre
-      dangerouslySetInnerHTML={{ __html: isPending ? 'pending' : codeResult }}
-    />
+    <Box position="relative" w="100%">
+      {highlightText !== '' ? (
+        <Button
+          size="xs"
+          onClick={onCopy}
+          position="absolute"
+          top="10px"
+          right="10px"
+        >
+          {hasCopied ? 'COPIED' : 'COPY'}
+        </Button>
+      ) : null}
+      {isPending ? (
+        <Stack>
+          <Skeleton height="20px" />
+          <Skeleton height="20px" />
+          <Skeleton height="20px" />
+        </Stack>
+      ) : (
+        <Pre
+          dangerouslySetInnerHTML={{
+            __html: highlightText,
+          }}
+        />
+      )}
+    </Box>
   );
 }
+
+const Pre = styled.pre`
+  pre.shiki {
+    padding: 10px;
+    overflow: auto;
+  }
+`;
